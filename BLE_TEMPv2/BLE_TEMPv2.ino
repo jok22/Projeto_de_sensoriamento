9b1c6e68fe44412d4c95623b2b2d62dcc4ea0fef
AntonioSilva584 <82547926+AntonioSilva584@users.noreply.github.com>
2021-12-10 20:22:36 -0300

    Add files via upload

---
+++ BLE_TEMPv2/BLE_TEMPv2.ino
@@ -0,0 +1,100 @@
+#include <BLEDevice.h>
+#include <BLEServer.h>
+#include <BLEUtils.h>
+#include <BLE2902.h>
+#include <iostream>
+#include <string>
+#include <Adafruit_MLX90614.h>
+
+Adafruit_MLX90614 mlx = Adafruit_MLX90614(); 
+BLEServer* pServer = NULL;
+BLECharacteristic* pCharacteristic = NULL;
+bool deviceConnected = false;
+bool oldDeviceConnected = false;
+float temperature; 
+const int LED = 2;
+
+#define SERVICE_UUID        "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
+#define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
+
+class MyServerCallbacks: public BLEServerCallbacks {
+    void onConnect(BLEServer* pServer) {
+      deviceConnected = true;
+      BLEDevice::startAdvertising();
+    };
+
+    void onDisconnect(BLEServer* pServer) {
+      deviceConnected = false;
+    }
+};
+
+void setup()
+{
+  Serial.begin(9600);
+  mlx.begin();
+  
+
+  BLEDevice::init("PROJETO DE SENSORIAMENTO");
+
+  pServer = BLEDevice::createServer();
+
+  pServer->setCallbacks(new MyServerCallbacks());
+
+  BLEService *pService = pServer->createService(SERVICE_UUID);
+
+  pCharacteristic = pService->createCharacteristic(
+                      CHARACTERISTIC_UUID,
+                      BLECharacteristic::PROPERTY_READ   |
+                      BLECharacteristic::PROPERTY_WRITE  |
+                      BLECharacteristic::PROPERTY_NOTIFY |
+                      BLECharacteristic::PROPERTY_INDICATE
+                    );
+
+
+  pCharacteristic->addDescriptor(new BLE2902());
+
+  pService->start();
+
+  BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
+  pAdvertising->addServiceUUID(SERVICE_UUID);
+  pAdvertising->setScanResponse(false);
+  pAdvertising->setMinPreferred(0x0); 
+  BLEDevice::startAdvertising();
+  Serial.println("Waiting a client connection to notify...");
+}
+ 
+void loop()
+{
+    temperature = mlx.readObjectTempC();
+    
+    if (isnan(temperature)) 
+    {
+      Serial.println("Failed to read from Sensor");
+    }
+    else
+    {
+      Serial.print("Temperatura: ");
+      Serial.print(temperature);
+      Serial.println(" *C");
+    }
+    if (deviceConnected) {
+        String str = "";
+        str += temperature;
+        pCharacteristic->setValue((char*)str.c_str());
+        pCharacteristic->notify();
+        delay(10); 
+    }
+    // disconnecting
+    if (!deviceConnected && oldDeviceConnected) {
+        delay(500);
+        pServer->startAdvertising(); 
+        Serial.println("start advertising");
+        oldDeviceConnected = deviceConnected;
+    }
+    // connecting
+    if (deviceConnected && !oldDeviceConnected) {
+        oldDeviceConnected = deviceConnected;
+    }
+Serial.println();
+delay(500);
+}
